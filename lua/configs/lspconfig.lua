b3c4e421e0021d23d010332bc68701385ffc4d3e
ianTevesAcc <iantevesbusinessacc@outlook.com>
2025-03-09 20:26:12 +1300

    Fixed lsp config where default configs were not being detected
    seperated root pattern

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,38 +1,31 @@
 -- Load defaults for Lua language server
-require("nvchad.configs.lspconfig").defaults()
-
 local lspconfig = require "lspconfig"
+
+-- Define root pattern
+local root_pattern = lspconfig.util.root_pattern(
+  ".luarc.json",
+  ".luarc.jsonc",
+  ".luacheckrc",
+  ".stylua.toml",
+  "stylua.toml",
+  "selene.toml",
+  "selene.yml",
+  ".git"
+)
+
+-- Example LSP servers
+local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- Lua language server configuration
-lspconfig.lua_ls.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  root_dir = function(fname)
-    -- Try to find a proper root using Git or Lua config files
-    local root = lspconfig.util.find_git_ancestor(fname)
-    if root then
-      return root -- If Git repository exists, use it as the root
-    end
-    -- Otherwise, fallback to finding Lua configuration files
-    local root_pattern = lspconfig.util.root_pattern(
-      ".luarc.json",
-      ".luarc.jsonc",
-      ".luacheckrc",
-      ".stylua.toml",
-      "stylua.toml",
-      "selene.toml",
-      "selene.yml",
-      ".git"
-    )
-    -- If no root config found, fallback to current working directory
-    return root_pattern(fname) or vim.fn.getcwd()
-  end,
-  single_file_support = true, -- Support for single Lua files
-}
+-- LSPs with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+    root_dir = root_pattern, -- Add root pattern here
+  }
+end
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
