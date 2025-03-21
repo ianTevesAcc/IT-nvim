0a7db193cea98578e4858b4a4ef47486c30aaaea
ianTevesAcc <iantevesbusinessacc@outlook.com>
2025-03-21 20:13:09 +1300

    2025-03-21 20:13:09

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,15 +2,34 @@ return {
   "nvim-tree/nvim-tree.lua",
   config = function()
     -- Ensure we are setting the correct initial directory
-    local initial_dir = vim.fn.isdirectory(vim.fn.argv(0)) == 1 and vim.fn.argv(0) or vim.fn.getcwd()
+    local initial_dir = vim.fn.isdirectory(vim.fn.argv(0)) == 1 and vim.fn.argv(0)
+      or vim.fn.getcwd()
     vim.cmd("cd " .. initial_dir)
 
-    require("nvim-tree").setup {
+    local nvim_tree = require "nvim-tree"
+    local api = require "nvim-tree.api"
+
+    -- Track dotfile visibility
+    local show_dotfiles = false
+
+    nvim_tree.setup {
       sync_root_with_cwd = true, -- Sync root directory with the current working directory
       hijack_directories = {
         enable = true, -- Automatically change to the directory of the opened file
       },
+      filters = {
+        dotfiles = true, -- Hide dotfiles by default
+      },
     }
+
+    -- Function to toggle dotfiles visibility
+    local function toggle_dotfiles()
+      show_dotfiles = not show_dotfiles
+      api.tree.toggle_hidden_filter() -- Properly toggles dotfiles visibility
+    end
+
+    -- Set keybinding to toggle dotfiles with <leader>k when in nvim-tree
+    vim.keymap.set("n", "<leader>k", toggle_dotfiles, { desc = "Toggle dotfiles in nvim-tree" })
 
     -- Auto-close nvim-tree if it's the last window open
     vim.api.nvim_create_autocmd("BufEnter", {
